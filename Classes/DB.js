98e59bc1633c99c04ca6b8f3be25e70926deff4d
jescg <jchavezg1300@alumno.ipn.mx>
2025-09-29 16:13:23 -0600

    Actualización de Dashboard

---
+++ Classes/DB.js
@@ -0,0 +1,12 @@
+class DB{
+    static conexion = null;
+
+    constructor (){
+        if(DB){
+
+        }else{
+            throw new console.error("error de conexión");
+            
+        }
+    }
+}
--- Classes/Validacion.js
+++ Classes/Validacion.js
@@ -3,12 +3,13 @@ class Validacion{
     static EsBoleta (Boleta){
         const expresionNumerico = /^[0-9]+$/;
 
-        if(expresionNumerico.test(Boleta) == true && Boleta.length== 10){
+        if(expresionNumerico.test(Boleta) == true && Boleta.length == 10){
             return true;
         }else{
             return false;
         }
     }
+
 }
 
 module.exports = Validacion;
